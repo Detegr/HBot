12e25812e915fa79475d7b5936fd93acbe6f8958
Detegr <detegr@gmail.com>
2012-05-26 00:37:49 +0300

    Made a module out of IRCConnection

---
+++ IRCConnection.hs
@@ -0,0 +1,33 @@
+module IRCConnection(mkConnection,doConnection,ircStr) where
+
+import Network.Socket
+import Debug.Trace
+
+ircStr :: String -> String
+ircStr s = s ++ "\r\n"
+
+connectionStrings :: String -> String -> [String]
+connectionStrings n rn = [ircStr $ intercalate rn ["USER "," "," * :",""],
+                          ircStr $ "NICK " ++ n]
+                                   
+debug = flip trace
+
+addrFromStr :: String -> IO HostAddress
+addrFromStr addr = getAddrInfo Nothing (Just addr) Nothing >>= \infos ->
+                   return $ getAddr (addrAddress . head $ infos)
+
+getAddr :: SockAddr -> HostAddress
+getAddr (SockAddrInet _ h) = h
+
+mkConnection :: String -> Int -> IO Socket
+mkConnection addr port = socket AF_INET Stream defaultProtocol >>= \sock ->
+                         addrFromStr addr >>= \hostaddr ->
+                         let sockaddr = (SockAddrInet (fromIntegral port :: PortNumber) hostaddr)
+                            in
+                            connect sock sockaddr >>
+                            putStrLn ("Connecting to " ++ (show sockaddr)) >>
+                            return sock `debug` "Connected"
+
+doConnection :: Socket -> String -> String -> IO()
+doConnection s nick realname
+    = mapM_ (\str -> send s str) $ connectionStrings nick realname
--- hbot.hs
+++ hbot.hs
@@ -1,37 +1,6 @@
+import IRCConnection
 import Network.Socket
-import Data.Word
 import Data.List
-import Debug.Trace
-import Data.Binary
-
-ircStr :: String -> String
-ircStr s = s ++ "\r\n"
-
-connectionStrings :: String -> String -> [String]
-connectionStrings n rn = [ircStr $ intercalate rn ["USER "," "," * :",""],
-                          ircStr $ "NICK " ++ n]
-                                   
-debug = flip trace
-
-addrFromStr :: String -> IO HostAddress
-addrFromStr addr = getAddrInfo Nothing (Just addr) Nothing >>= \infos ->
-                   return $ getAddr (addrAddress . head $ infos)
-
-getAddr :: SockAddr -> HostAddress
-getAddr (SockAddrInet _ h) = h
-
-mkConnection :: String -> Int -> IO Socket
-mkConnection addr port = socket AF_INET Stream defaultProtocol >>= \sock ->
-                         addrFromStr addr >>= \hostaddr ->
-                         let sockaddr = (SockAddrInet (fromIntegral port :: PortNumber) hostaddr)
-                            in
-                            connect sock sockaddr >>
-                            putStrLn ("Connecting to " ++ (show sockaddr)) >>
-                            return sock `debug` "Connected"
-
-doConnection :: Socket -> String -> String -> IO()
-doConnection s nick realname
-    = mapM_ (\str -> send s str) $ connectionStrings nick realname
 
 loop :: Socket -> IO()
 loop s = recv s 4096 >>= \str ->
