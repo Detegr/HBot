d4eb366e9e95c5c24f12494943e1d82072fc7e0e
Detegr <detegr@gmail.com>
2014-11-30 14:03:21 +0200

    Rewrote Unicafe plugin to use their new json-api

--- Plugin/Unicafe.hs
+++ Plugin/Unicafe.hs
@@ -1,36 +1,75 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell,OverloadedStrings,ScopedTypeVariables #-}
 
 module Plugin.Unicafe(unicafe) where
 
-import Text.HTML.TagSoup hiding (parseTags, renderTags)
-import Text.HTML.TagSoup.Fast.Utf8Only
-import qualified Data.Text as T
-import qualified Data.ByteString.Char8 as B
-import Network.HTTP
-import Text.Parsec
-import Text.Parsec.Text
-import Control.Monad
-import Data.Dates
-import Data.Time.Calendar.WeekDate (toWeekDate)
-import qualified Data.Text.IO (putStrLn)
-import Data.List (foldl')
-import Data.Char
-import Data.Maybe (catMaybes)
+import Control.Applicative ((<$>), (<*>))
 import Data.List (intercalate)
-import Debug.Trace
+import Data.Char (toLower)
+import Data.Aeson
+import Data.Aeson.TH
+import Data.DateTime
+import qualified Data.Text as T hiding (map,concat,filter)
+import Network.HTTP
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.Text.IO as TIO
 
-import Parser
-import Connection
 import PluginData
+import Data.Maybe (catMaybes)
 
-restaurants :: [String]
-restaurants = ["Metsätalo", "Olivia", "Porthania", "Päärakennus", "Rotunda", "Sockom",
-               "Topelias", "Valtiotiede", "Ylioppilasaukio", "Kookos", "Chemicum", "Exactum",
-               "Physicum", "Meilahti", "Ruskeasuo", "Biokeskus", "Korona", "Viikuna"]
-
-data Restaurant = Metsatalo | Olivia | Porthania | Paarakennus | Rotunda | SocKom | Topelias | Valtiotiede | Ylioppilasaukio |
-                  Kookos | Chemicum | Exactum | Physicum | Meilahti | Ruskeasuo | Biokeskus | Korona | Viikuna
+data Restaurant = Metsatalo | Olivia | Porthania | Paarakennus | Rotunda | SocKom | Topelias | Valtiotiede |
+                  Kookos | Chemicum | Exactum | Physicum | Meilahti | Ruskeasuo | Biokeskus | Korona | Viikuna | Ylioppilasaukio
                   deriving (Show,Bounded,Eq,Read)
+
+restaurantStrings :: [String]
+restaurantStrings = ["Metsätalo", "Olivia", "Porthania", "Päärakennus", "Rotunda", "Sockom",
+            "Topelias", "Valtiotiede", "Ylioppilasaukio", "Kookos", "Chemicum", "Exactum",
+            "Physicum", "Meilahti", "Ruskeasuo", "Biokeskus", "Korona", "Viikuna"]
+
+data UnicafeData = UnicafeData
+  { main_status :: T.Text
+  , main_data :: [UnicafeDay]
+  } deriving (Show, Eq)
+
+data UnicafeFood = UnicafeFood
+  { food_name :: T.Text
+  , food_price :: UnicafePrice
+  } deriving (Show, Eq)
+
+data UnicafePrice = UnicafePrice
+  { price_name :: T.Text
+  } deriving (Show, Eq)
+
+data UnicafeDay = UnicafeDay
+  { day_data :: [UnicafeFood]
+  , day_date :: T.Text
+  } deriving (Show, Eq)
+
+days :: UnicafeData -> [UnicafeDay]
+days = main_data
+
+foods :: UnicafeDay -> [UnicafeFood]
+foods = day_data
+
+foodName :: UnicafeFood -> T.Text
+foodName = food_name
+
+foodPrice :: UnicafeFood -> T.Text
+foodPrice = price_name . food_price
+
+foodFilter :: UnicafeFood -> Bool
+foodFilter f = foodPrice f == "Edullisesti"
+
+dateFilter :: T.Text -> UnicafeDay -> Bool
+dateFilter date day = (T.drop 3 $ day_date day) == date
+
+foodNames :: T.Text -> UnicafeData -> [T.Text]
+foodNames date = concat . map ((map foodName . filter foodFilter)) . map foods . filter (dateFilter date) . days
+
+$(deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 5} ''UnicafeData)
+$(deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 4} ''UnicafeDay)
+$(deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 5} ''UnicafeFood)
+$(deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 6} ''UnicafePrice)
 
 -- This declaration feels very stupid, but quick googling resulted no better alternative...
 instance Enum Restaurant where
@@ -74,7 +113,7 @@ instance Enum Restaurant where
 
 -- Gotta learn how to write a Read instance :D
 strToRestaurant :: String -> Maybe Restaurant
-strToRestaurant s = go $ map toLower s
+strToRestaurant s = go $ Prelude.map toLower s
   where go "metsätalo"       = Just Metsatalo
         go "olivia"          = Just Olivia
         go "porthania"       = Just Porthania
@@ -95,64 +134,41 @@ strToRestaurant s = go $ map toLower s
         go "viikuna"         = Just Viikuna
         go _                 = Nothing
 
-unicafeurl :: Int -> Int -> Integer -> Restaurant -> String
-unicafeurl w d y id = "http://www.unicafe.fi/lounastyokalu/index.php?option=com_ruokalista&Itemid=29&task=lounaslista_haku&week=" ++
-                      show w ++ "&day=" ++ show d ++ "&year=" ++ show y ++ "&rid=" ++ show (fromEnum id) ++ "&lang=1"
+unicafeUrl :: Restaurant -> String
+unicafeUrl r = "http://hyy-lounastyokalu-production.herokuapp.com/publicapi/restaurant/" ++ (show . fromEnum $ r)
 
-source addr = simpleHTTP (getRequest addr) >>= getResponseBody
+source :: String -> IO B.ByteString
+source addr = simpleHTTP (getRequest addr) >>= (fmap C.pack) . getResponseBody
 
-foodParser :: Parser T.Text
-foodParser = do
-  f <- many $ try fp'
-  t <- try $ string "Edullisesti" <|> string "Kevyesti"
-  return $ T.pack (concat f)
- where fp' = do
-             str <- many $ noneOf "(["
-             try $ do
-               many $ noneOf ")"
-               char ')'
-               return () <|> eof
-             return str
+source' :: IO B.ByteString
+source' = B.readFile "../unicafe.json"
 
-debug = flip trace
+printFood :: (Restaurant, [T.Text]) -> [String]
+printFood (r, foods)
+  | Prelude.length foods == 0 = [separator '-', show r ++ ":", "No food available today."]
+  | otherwise = [separator '-', show r ++ ":"] ++ map T.unpack foods
 
-joinFoodAndType :: [T.Text] -> [T.Text]
-joinFoodAndType [] = []
-joinFoodAndType [f] =
-  case parse foodParser "" f of
-    Left err -> [] `debug` show err
-    Right f  -> [f] `debug` (T.unpack f)
-joinFoodAndType (f:t:xs) =
-  case parse foodParser "" (T.concat [f, t]) of
-    Left err -> joinFoodAndType xs `debug` (T.unpack f)
-    Right f -> [f] ++ joinFoodAndType xs `debug` (T.unpack f)
+separator :: Char -> String
+separator c = Prelude.take 19 $ repeat c
 
-foodsFromSource :: String -> [String]
-foodsFromSource = Prelude.map T.unpack . joinFoodAndType .
-                  Prelude.filter ((> 1) . T.length) .
-                  Prelude.map fromTagText .
-                  Prelude.filter isTagText .
-                  dropWhile (not . isTagOpenName "li") .
-                  parseTagsT . B.pack
-
-header :: DateTime -> String
-header dt = "Food for " ++ (show . day $ dt) ++ "." ++ (show . month $ dt) ++ "." ++ (show . year $ dt)
-
-foodsForRestaurant :: Int -> Int -> Integer -> Restaurant -> IO [String]
-foodsForRestaurant w wd y r = fmap foodsFromSource (source $ unicafeurl w wd y r)
-
-spacer :: Char -> String
-spacer c = take 19 $ repeat c
-
-specialSpacer :: Char -> Char -> String
-specialSpacer c s = concat [take 9 normal, [s], take 9 normal]
+specialSeparator :: Char -> Char -> String
+specialSeparator c s = concat [take 9 normal, [s], take 9 normal]
   where normal = repeat c
 
-restaurantHeader :: Restaurant -> [String] -> [String]
-restaurantHeader r f = [spacer '-', show r ++ ":"] ++ f
+currentDate :: IO T.Text
+currentDate = toUnicafeDate <$> toGregorian' <$> getCurrentTime
 
-restaurantHeaders :: [Restaurant] -> [[String]] -> [[String]]
-restaurantHeaders rs fds = map (\(r,f) -> restaurantHeader r f) $ zip rs fds
+toUnicafeDate :: (Integer, Int, Int) -> T.Text
+toUnicafeDate (y,m,d) = T.pack $ intercalate "." [show d, show m]
+
+getFoods :: [Restaurant] -> IO [(Restaurant, [T.Text])]
+getFoods = mapM getFood
+ where getFood r = do
+       src <- source . unicafeUrl $ r
+       date <- currentDate
+       return $ case eitherDecode src of
+         Right (s :: UnicafeData) -> (r, foodNames date s)
+         Left err -> (r, [T.pack err])
 
 getRestaurants :: PluginData a -> [Restaurant]
 getRestaurants pd =
@@ -165,14 +181,15 @@ getRestaurants pd =
 
 usage :: [String]
 usage = ["Available restaurants: ",
-         intercalate ", " restaurants,
+         intercalate ", " restaurantStrings,
          "Example usage: !unicafe Porthania Päärakennus"]
 
 unicafe :: PluginData a -> IO (PluginResult a)
 unicafe pd = do
-  dt <- getCurrentDateTime
-  let (year, week, weekday) = toWeekDate . dateTimeToDay $ dt
-  foods <- mapM (foodsForRestaurant week weekday year) $ getRestaurants pd
-  case foods of
+  (d,m,y) <- toGregorian' <$> getCurrentTime
+  case getRestaurants pd of
     []  -> msgsToChannel pd $ usage
-    fds -> msgsToChannel pd (Data.List.foldl' (\a s -> a ++ s) [header dt] $ (restaurantHeaders (getRestaurants pd) fds) ++ [[specialSpacer '-' '*']])
+    restaurants -> do
+      foods <- getFoods restaurants
+      let foodstrs = ["Food for " ++ intercalate "." [show d,show m,show y] ++ ":"] ++ (concat . map printFood $ foods) ++ [specialSeparator '-' '*']
+      msgsToChannel pd foodstrs
